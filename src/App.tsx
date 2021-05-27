dc0f7aac14615057bde72e721ff7b29ad16a8801
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2021-05-27 21:52:01 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+import {BackgroundAnimation} from "./components/BackgroundAnimation";
+
+function App() {
+    return (
+        <div className="App">
+            <BackgroundAnimation/>
+        </div>
+    );
+}
+
+export default App;
